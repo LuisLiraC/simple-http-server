5dc468c926f017133342198d127fdb77a12c5107
LuisLiraC <luis_ssbm@hotmail.com>
2023-10-16 23:13:35 -0600

    receiving requests

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+use std::net::TcpListener;
 pub struct Server {
     address: String,
 }
@@ -10,6 +12,22 @@ impl Server {
     }
 
     pub fn run(&self) {
+        let listener = TcpListener::bind(&self.address).unwrap();
         println!("Listening on: {}", self.address);
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        },
+                        Err(e) => println!("Error reading buffer: {}", e)
+                    }
+                },
+                Err(e) => println!("Failed to stablish connection {}", e)
+            }
+        }
     }
 }
