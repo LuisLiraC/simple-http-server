319ab36268706c2ed99377cfc9e51099d821ecdf
LuisLiraC <luis_ssbm@hotmail.com>
2023-10-17 10:41:44 -0600

    test signed commit vscode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use http_server::Server;
 
 fn main() {
-    let server = Server::new("127.0.0.1:5000".to_string());
+    let server = Server::new("127.0.0.1:4000".to_string());
     server.run();
 }
